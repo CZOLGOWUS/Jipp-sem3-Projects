00cd308917376c22116b892483a41aca96ea6f47
CZOLGOWUS <jkolodziej58@outlook.com>
2020-12-03 23:20:37 +0100

    adding lab5(exeptions)

--- labs/lab5/src/Matrix.cpp
+++ labs/lab5/src/Matrix.cpp
@@ -130,11 +130,18 @@ Matrix::Matrix(std::string path)
 }
 Matrix::~Matrix()
 {
-    for (int i = 0; i < _xSize; i++)
+    try
     {
-        delete[] _matrix[i];
+        for (int i = 0; i < _xSize; i++)
+        {
+            delete[] _matrix[i];
+        }
+            delete[] _matrix;
     }
-        delete[] _matrix;
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << "\n";
+    }
 }
 
 //coping constructor
@@ -144,11 +151,20 @@ Matrix::Matrix(const Matrix& m1)
     this->_xSize = m1._xSize;
     this->_ySize = m1._ySize;
 
-    for (int i = 0; i < this->_xSize; i++)
-        for (int j = 0; j < this->_ySize; j++)
-        {
-            this->_matrix[i][j] = m1._matrix[i][j];
-        }
+    try
+    {
+        for (int i = 0; i < this->_xSize; i++)
+            for (int j = 0; j < this->_ySize; j++)
+            {
+                this->_matrix[i][j] = m1._matrix[i][j];
+            }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+
 }
 
 //Matrixis in making
@@ -169,10 +185,16 @@ double** Matrix::InstanciateMatrix(int x, int y)
 //Operations returning pointers
 Matrix* Matrix::addReturnPointer(const Matrix& m2 ) const
 {
-    if ( !(this->_xSize == m2._xSize && this->_ySize == m2._ySize) )
+    try
     {
-        std::cout << "matrixies cannot be added : diffrent sizes\n";
-        exit(-1);
+        if ( !(this->_xSize == m2._xSize && this->_ySize == m2._ySize) )
+        {
+            throw std::invalid_argument("matrixies cannot be added : diffrent sizes");
+        }
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << "\n";
     }
 
     Matrix* AddedM = new Matrix(this->_xSize, this->_ySize);
@@ -187,10 +209,16 @@ Matrix* Matrix::addReturnPointer(const Matrix& m2 ) const
 }
 Matrix* Matrix::subbReturnPointer(const Matrix& m2) const
 {
-    if (!(this->_xSize == m2._xSize && this->_ySize == m2._ySize))
+    try
     {
-        std::cout << "matrixies cannot be added : diffrent sizes\n";
-        exit(-1);
+        if ( !(this->_xSize == m2._xSize && this->_ySize == m2._ySize) )
+        {
+            throw std::invalid_argument("matrixies cannot be added : diffrent sizes");
+        }
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << "\n";
     }
 
     Matrix* SubbM = new Matrix(this->_xSize, this->_ySize);
@@ -205,10 +233,16 @@ Matrix* Matrix::subbReturnPointer(const Matrix& m2) const
 }
 Matrix* Matrix::multiReturnPointer(const  Matrix& m2 ) const
 {
-    if (m2._xSize != this->_ySize )
+    try
     {
-        std::cout << "matrixies cannot be added : diffrent sizes\n";
-        exit(-1);
+        if (m2._xSize != this->_ySize )
+        {
+            throw std::invalid_argument("matrixies cannot be added : this.number_of_columns is not equal to secondMatrix.number_of_rows");
+        }
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << "\n";
     }
 
     Matrix* MultiM = new Matrix( this->_xSize , m2._ySize , (double)0);
@@ -227,10 +261,16 @@ Matrix* Matrix::multiReturnPointer(const  Matrix& m2 ) const
 //Operations returning copies
 Matrix Matrix::addReturnCopy(const Matrix& m2) const
 {
-    if (!(this->_xSize == m2._xSize && this->_ySize == m2._ySize))
+    try
     {
-        std::cout << "matrixies cannot be added : diffrent sizes\n";
-        exit(-1);
+        if ( !(this->_xSize == m2._xSize && this->_ySize == m2._ySize) )
+        {
+            throw std::invalid_argument("matrixies cannot be added : diffrent sizes");
+        }
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << "\n";
     }
 
     Matrix AddedM(this->_xSize, this->_ySize);
@@ -245,10 +285,16 @@ Matrix Matrix::addReturnCopy(const Matrix& m2) const
 }
 Matrix Matrix::subbReturnCopy(const Matrix& m2) const
 {
-    if (!(this->_xSize == m2._xSize && this->_ySize == m2._ySize))
+    try
     {
-        std::cout << "matrixies cannot be added : diffrent sizes\n";
-        exit(-1);
+        if ( !(this->_xSize == m2._xSize && this->_ySize == m2._ySize) )
+        {
+            throw std::invalid_argument("matrixies cannot be added : diffrent sizes");
+        }
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << "\n";
     }
 
     Matrix SubbM(this->_xSize, this->_ySize);
@@ -263,10 +309,16 @@ Matrix Matrix::subbReturnCopy(const Matrix& m2) const
 }
 Matrix Matrix::multiReturnCopy(const Matrix& m2) const
 {
-    if (m2._xSize != this->_ySize )
+    try
     {
-        std::cout << "matrixies cannot be added : diffrent sizes\n";
-        exit(-1);
+        if (m2._xSize != this->_ySize )
+        {
+            throw std::invalid_argument("matrixies cannot be added : this.number_of_columns is not equal to secondMatrix.number_of_rows");
+        }
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << "\n";
     }
 
     Matrix MultiM( this->_xSize , m2._ySize , (double)0);
@@ -285,10 +337,16 @@ Matrix Matrix::multiReturnCopy(const Matrix& m2) const
 //operators
 Matrix Matrix::operator-(const Matrix& m2) const
 {
-    if (!(this->_xSize == m2._xSize && this->_ySize == m2._ySize))
+    try
     {
-        std::cout << "matrixies cannot be added : diffrent sizes\n";
-        exit(-1);
+        if ( !(this->_xSize == m2._xSize && this->_ySize == m2._ySize) )
+        {
+            throw std::invalid_argument("matrixies cannot be added : diffrent sizes");
+        }
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << "\n";
     }
 
     Matrix SubbMatrix(this->_xSize, this->_ySize);
@@ -301,12 +359,18 @@ Matrix Matrix::operator-(const Matrix& m2) const
 
     return SubbMatrix;
 }
-Matrix Matrix::operator+(const Matrix& M2) const 
+Matrix Matrix::operator+(const Matrix& m2) const 
 {
-    if (_xSize != M2._xSize || _ySize != M2._ySize) 
+    try
     {
-        std::cout << "Dimensions don't match!" << std::endl;
-        exit(-1);
+        if ( !(this->_xSize == m2._xSize && this->_ySize == m2._ySize) )
+        {
+            throw std::invalid_argument("matrixies cannot be added : diffrent sizes");
+        }
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << "\n";
     }
 
     Matrix new_mat(_xSize, _ySize);
@@ -314,10 +378,36 @@ Matrix Matrix::operator+(const Matrix& M2) const
     {
         for (int j = 0; j < _ySize; j++) 
         {
-            new_mat._matrix[i][j] = _matrix[i][j] + M2._matrix[i][j];
+            new_mat._matrix[i][j] = _matrix[i][j] + m2._matrix[i][j];
         }
     }
     return new_mat;
+}
+Matrix Matrix::operator*(const Matrix& m2) const 
+{
+    try
+    {
+        if (m2._xSize != this->_ySize )
+        {
+            throw std::invalid_argument("matrixies cannot be added : this.number_of_columns is not equal to secondMatrix.number_of_rows");
+        }
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << "\n";
+    }
+
+    Matrix MultiM( this->_xSize , m2._ySize , (double)0);
+
+    for (int i = 0; i < this->_xSize; i++)
+    {
+        for (int j = 0; j < m2._ySize; j++)
+        {
+            for (int k = 0; k < this->_ySize ; k++)
+                MultiM._matrix[i][j] += this->get(i,k) * m2.get(k,j);
+        }
+    }
+    return MultiM;
 }
 void Matrix::operator = (const Matrix& m1)
 {
@@ -340,22 +430,33 @@ void Matrix::operator = (const Matrix& m1)
 //defult methods
 void Matrix::set(int x = 0, int y = 0, double val = 0)
 {
-    if (x < _xSize && y < _ySize)
-        _matrix[x][y] = val;
-    else
+    try
     {
-        std::cout << "set() error : out of bounds exeption\n";
-        exit(-1);
+        _matrix[x][y] = val;
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << "\n";
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "\n";
     }
 }
 double Matrix::get(int x, int y) const
 {
-    if (x < _xSize && y < _ySize && x >= 0 && y >= 0)
-        return _matrix[x][y];
-    else
+    try
     {
-        std::cout << "get() error : out of bounds exeption\n";
-        exit(-1);
+        return _matrix[x][y];
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << "\n";
+
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "\n";
     }
 }
 void Matrix::PrintMatrix()const
@@ -365,7 +466,7 @@ void Matrix::PrintMatrix()const
     for (int i = 0; i < _xSize; i++)
         for (int j = 0; j < _ySize; j++)
         {
-            k = std::to_string(_matrix[i][j]).length();
+            k = std::to_string(this->get(i,j)).length();
             if (k > largerstNumber)
             {
                 largerstNumber = k;
@@ -377,7 +478,7 @@ void Matrix::PrintMatrix()const
         std::cout << "| ";
         for (int j = 0; j < _ySize; j++)
         {
-            std::cout << std::setw(largerstNumber - 5) << _matrix[i][j] << " ";
+            std::cout << std::setw(largerstNumber - 5) << this->get(i,j) << " ";
         }
         std::cout << " |\n";
     }
@@ -395,24 +496,51 @@ void Matrix::store(std::string filename, std::string path) const
 {
     std::ofstream file;
 #ifdef _WIN32
+try
+{
     file.open(path + "//" + filename );
+}
+catch(const std::exception& e)
+{
+    std::cerr << e.what() << "\n";
+}
 #else
+try
+{
     file.open(path + "/" + filename );
+}
+catch(const std::exception& e)
+{
+    std::cerr << e.what() << "\n";
+}
 #endif
 
-    if (file.is_open())
-    {
         std::string placeHolderString;
-        file << '/' << _xSize << '/' << _ySize <<'|';
+        try
+        {
+            file << '/' << _xSize << '/' << _ySize <<'|';
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << "\n";
+        }
+
         for (int x = 0; x < _xSize; x++)
             for (int y = 0; y < _ySize; y++)
             {
-                placeHolderString.append(std::to_string(_matrix[x][y]) + '|');
+                placeHolderString.append(std::to_string(this->get(x,y)) + '|');
             }
-        file << placeHolderString;
-        file.close();
-        return;
-    }
+
+        try
+        {
+            file << placeHolderString;
+            file.close();
+            return;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << "\n";
+        }
 
     std::cout << "file : " << filename << " could not be opened\n";
     exit(-1);
---
+++ labs/lab5/src/main.cpp
@@ -0,0 +1,138 @@
+#include <Matrix.hpp>
+
+
+void test()
+{
+    std::cout << "creating Matrixsies:\n";
+    Matrix M1;
+    Matrix M2(5);
+    Matrix M3(4, 5,1);
+    Matrix M4(4, 5,2);
+    Matrix Mm1(6,14,3);
+    Matrix Mm2(14,6, 4);
+
+    std::cout << "Printing created Matrixsies:\n";
+    M1.PrintMatrix();
+    M2.PrintMatrix();
+    M3.PrintMatrix();
+    M4.PrintMatrix();
+    Mm1.PrintMatrix();
+    Mm2.PrintMatrix();
+    std::cout << "\n\n\n\n";
+
+
+    std::cout << "Adding Matrix: \n";
+    M3.PrintMatrix();
+    std::cout << "\n";
+    M4.PrintMatrix();
+    std::cout << "\n";
+
+
+    Matrix* M3M4 = M3.addReturnPointer(M4); // dodaje do M3, M4
+    M3M4->PrintMatrix();
+    std::cout << "\n";
+
+    std::cout << "Substracting Matrix: \n";
+    M3.PrintMatrix();
+    std::cout << "\n";
+    M4.PrintMatrix();
+    std::cout << "\n";
+
+
+    Matrix* MSubstructed = M3.subbReturnPointer(M4);
+    MSubstructed->PrintMatrix();
+    std::cout << "\n";
+
+    std::cout << "Operator + \n";
+    Matrix M8 = M3 + M4;
+    M8.PrintMatrix();
+
+    std::cout << "Get() and set() in (0,0) Matrix:\n";
+    M4.PrintMatrix();
+    std::cout << "\n";
+    std::cout << "setting (0,0) to 654546522231\n";
+    M4.set(0, 0, 654546522231);
+    M4.PrintMatrix();
+    std::cout << "\n";
+    std::cout << M4.get(0, 0) << std::endl;
+
+    std::cout << "Multiplication of 2 Matrixies:\n";
+    Mm1.PrintMatrix();
+    std::cout << "\n";
+    Mm2.PrintMatrix();
+    std::cout << "\n\n\n";
+    Matrix* Mm3 = Mm1.multiReturnPointer(Mm2);
+    Mm3->PrintMatrix();
+    std::cout << "\n\n\n";
+
+//Zapis i odczyt macierzy:
+    Matrix test(12, 32);
+    for (int i = 0; i < test.rows(); i++)
+        for (int j = 0; j < test.cols(); j++)
+            test.set(i, j, (double)10 * i * j * 0.54);
+    std::cout << "Save Matrix:\n";
+    test.PrintMatrix();
+    std::cout << "\n\n\n";
+
+#ifdef _WIN32
+    test.store("test.txt", "E:\\studia\\semestr 3\\JiPP\\labs\\1\\visualcommunity porject\\tests");
+#else
+    test.store("test.txt", "./");
+#endif
+
+    std::cout << "Load Matrix:\n";
+    std::cout << "\n\n\n";
+
+#ifdef _WIN32
+    Matrix test2("E:\\studia\\semestr 3\\JiPP\\labs\\1\\visualcommunity porject\\tests\\test.txt");
+#else
+    Matrix test2("./test.txt");
+#endif
+
+    test2.PrintMatrix();
+
+
+    std::cout << "\n\n\n";    
+    std::cout << "\nTesting copying constructors:\n";
+
+    Matrix a1(5, 7, 3);
+    Matrix a2(5, 7, 1);
+
+    std::cout << "a1: \n";
+    a1.PrintMatrix();
+
+    std::cout << "a2: \n";
+    a2.PrintMatrix();
+
+
+
+    std::cout << "\nAdding\n";
+    a1 = a1 + a2;
+
+    std::cout << "a1: \n";
+    a1.PrintMatrix();
+
+    std::cout << "a2: \n";
+    a2.PrintMatrix();
+    
+
+
+    std::cout << "\nSubbstracting\n";
+    a1 = a1 - a2;
+
+    std::cout << "a1: \n";
+    a1.PrintMatrix();
+
+    std::cout << "a2: \n";
+    a2.PrintMatrix();
+
+
+}
+
+int main()
+{
+
+	test();
+
+	return 0;
+}
